62a7e29303d8df70199974219f73411f852bf2ee
Shivansh Guleria <shivanshguleria@proton.me>
2024-06-07 10:20:13 +0530

    Update database.py

--- app/danych/database.py
+++ app/danych/database.py
@@ -1,7 +1,8 @@
+# Dependency
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from app.danych.get_cred import Credentials
+from app.danych.get_cred import Credentials # get credentials
 # "postgresql://root:root@localhost/postgres"
 
 engine = create_engine(
@@ -14,8 +15,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependency
 def get_db():
+    """
+        Returns  db instance from pool
+    """
     db = SessionLocal()
     print("[INFO] 🗄️  🚀🚀 Postgres DB connected")
     try:
